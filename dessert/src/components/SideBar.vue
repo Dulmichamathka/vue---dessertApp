8c1636c38e54c59e5546bc8a0967cbcf9f718567
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-11-20 21:21:02 +0530

    initial commit

---
+++ dessert/src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <v-navigation-drawer dark app color="#FFCB5E" width="100">
+      <div class="text-center mt-5">
+        <v-icon color="#4D2C00" large class="mt-4">fas fa-bars</v-icon>
+      </div>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data: () => ({
+    selectedItem: 0,
+    drawer: null,
+    items: [
+      { icon: "fas fa-apple-alt", text: "Discover" },
+      { icon: "fas fa-carrot", text: "Stream" },
+      { icon: "fas fa-leaf", text: "Community" },
+      { icon: "fas fa-lemon", text: "Statistics" },
+      { icon: "fas fa-pepper-hot", text: "Statistics" },
+      { icon: "fas fa-seedling", text: "Statistics" },
+    ],
+  }),
+};
+</script>
+
+<style></style>
---
+++ dessert/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <SideBar />
+    <v-container> </v-container>
+  </v-app>
+</template>
+
+<script>
+import SideBar from "../components/SideBar";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    SideBar,
+  },
+};
+</script>
